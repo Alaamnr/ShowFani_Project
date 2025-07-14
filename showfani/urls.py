8ab95905b5ef1a8ee8e081ee42ae99fc3d67060f
Alaa Almansour <alaaleolmansour>
2025-07-14 19:27:15 +0300

    Fix URL patterns

--- showfani/urls.py
+++ showfani/urls.py
@@ -18,6 +18,7 @@ schema_view = get_schema_view(
       license=openapi.License(name="BSD License"),
    ),
    public=True,
+   authentication_classes=(),
    permission_classes=(permissions.AllowAny,),
 )
 urlpatterns = [
