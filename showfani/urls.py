5f86a438cdd2fee273a323902600010ad36ca644
Alaa Almansour <alaaleolmansour>
2025-07-28 04:04:34 +0300

    AF

--- showfani/urls.py
+++ showfani/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
    
@@ -16,8 +16,10 @@ urlpatterns = [
     path('api/search/', include('search.urls', namespace='search')),
     path('api/app/', include('app.urls', namespace='app')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/token/', include('rest_framework_simplejwt.urls')),
-
+    #path('api/token/', include('rest_framework_simplejwt.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
