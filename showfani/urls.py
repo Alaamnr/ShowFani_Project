45f5fb0203185b8f61b3fa6d071b2f66218f8e6f
Alaa Almansour <alaaleolmansour>
2025-07-28 04:16:32 +0300

    y

--- showfani/urls.py
+++ showfani/urls.py
@@ -7,30 +7,27 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-   
+
+    path('api/users/', include('users.urls')),
+    path('api/posts/', include('posts.urls')),
+    path('api/chat/', include('chat.urls')),
+    path('api/filters/', include('filters.urls')),
+    path('api/search/', include('search.urls')),
+    path('api/app/', include('app.urls')), 
     
-    path('api/users/', include('users.urls', namespace='users')),
-    path('api/posts/', include('posts.urls', namespace='posts')),
-     path('api/chat/', include('chat.urls', namespace='chat')),
-    path('api/filters/', include('filters.urls', namespace='filters')),
-    path('api/search/', include('search.urls', namespace='search')),
-    path('api/app/', include('app.urls', namespace='app')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    #path('api/token/', include('rest_framework_simplejwt.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
     # Optional UI:
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
-     path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
-
-
-
