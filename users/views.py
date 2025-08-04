e32e58d818b34325d6b74f5185d56acbc703b182
Alaa Almansour <alaaleolmansour>
2025-08-04 21:06:40 +0300

    Fix token

--- users/views.py
+++ users/views.py
@@ -11,6 +11,8 @@ from .serializers import (
 from .models import CustomUser, Artist, Investor
 import cloudinary.uploader
 from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 from rest_framework.parsers import MultiPartParser
 class ArtistRegisterView(generics.CreateAPIView):
@@ -76,3 +78,36 @@ class TestUploadView(APIView):
         file = request.FILES.get('file')
         result = cloudinary.uploader.upload(file)
         return Response({'url': result['secure_url']})
+class CustomTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        try:
+            # جربي تجيبي التوكن الجديد
+            new_access_token = response.data.get('access')
+
+            if new_access_token:
+                # افكي تشفير التوكن وجيبي user_id
+                from rest_framework_simplejwt.tokens import AccessToken
+                access_token_obj = AccessToken(new_access_token)
+                user_id = access_token_obj['user_id']
+
+      
+                user = CustomUser.objects.get(id=user_id)
+
+
+                profile_data = None
+                if user.user_type == 'ARTIST':
+                    profile = user.artist_profile
+                    profile_data = ArtistProfileSerializer(profile).data
+                elif user.user_type == 'INVESTOR':
+                    profile = user.investor_profile
+                    profile_data = InvestorProfileSerializer(profile).data
+
+                response.data['user_type'] = user.user_type
+                response.data['profile'] = profile_data
+        except Exception as e:
+            print(f"Error in CustomTokenRefreshView: {e}")
+
+        return response
+
